dd5400e8c3a45d17edb4b3031ab988a1c2ae456d
Dominic Torruellas <dom@domtorr.com>
2019-06-30 12:13:38 -0700

    initial reducer setup

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,12 @@
+// Import actions
+
+const initialState = {
+  friends: [],
+}
+
+export default (state = initialState, {type, payload}) => {
+  switch(type) {
+    default:
+      return state
+  }
+}
